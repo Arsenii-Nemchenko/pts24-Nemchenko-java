0d5151d18e0aab0a3736dd1b7ae6cc59020f72a7
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2024-12-02 19:33:48 +0200

    Fixed EndOfGameEffect location

--- src/main/java/sk/uniba/fmph/dcs/stone_age/EndOfGameEffect.java
+++ src/main/java/sk/uniba/fmph/dcs/stone_age/EndOfGameEffect.java
@@ -1,5 +1,17 @@
-package sk.uniba.fmph.dcs.stone_age;
+package sk.uniba.fmph.dcs.player_board;
 
 public enum EndOfGameEffect {
-    FARMER, TOOL_MAKER, BUILDER, SHAMAN, MEDICINE, ART, MUSIC, WRITING,SUNDIAL, POTTERY, TRANSPORT, WEAVING
+    Farmer,
+    ToolMaker,
+    Builder,
+    Shaman,
+    Medicine,
+    Art,
+    Music,
+    Writing,
+    Sundial,
+    Pottery,
+    Transport,
+    Weaving
+
 }
